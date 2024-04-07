19b490a2facbf136670a29384d6e12169181b044
Kyle Andrews <codingwithkyle@gmail.com>
2024-04-07 09:40:42 -0400

    🏗 Added basic axum server with askama template rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+    routing::get,
+    Router,
+};
+use serde::{Deserialize, Serialize};
+use askama_axum::Template;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> HelloTemplate<'static> {
+    HelloTemplate { name: "mom" }
+}
+
+#[derive(Template)]
+#[template(path = "hello.twig.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
 }
